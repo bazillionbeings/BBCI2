b9f9a516ad7eca3451ff39693fc379c33434398f
Ara Yeressian <ayeressian2@gmail.com>
2015-12-09 15:26:59 +0400

    fixes

--- pull.sh
+++ pull.sh
@@ -2,6 +2,8 @@
 #set +e
 
 APP_ADDRESS=$1
+MYSQL_USER="root"
+MYSQL_PASSWORD="root"
 
 if [ "$APP_ADDRESS" = "" ]; then
   printf "Please provide application address as a first argument.\n"
@@ -10,32 +12,28 @@ fi
 
 cd $APP_ADDRESS
 
-printf "killall node\n"
-killall node
-
 printf "git fetch origin\n"
 git fetch origin
-printf "git checkout dev\n"
-git checkout dev
+
+#printf "git checkout dev\n"
+#git checkout dev
 printf "git diff origin/dev\n"
 if [[ $(git diff origin/dev) ]]; then
+  printf "killall node\n"
+  killall node
   printf "git rebase origin/dev\n"
   git rebase origin/dev
+
+  cd database
+
+  printf "mysql --user=\"root\" --password=\"root\" < \"./drop_database.sql\"\n"
+  mysql --user="root" --password="root" < "./drop_database.sql"
+  printf "mysql --user=\"root\" --password=\"root\" < \"./database_schema.sql\"\n"
+  mysql --user="root" --password="root" < "./database_schema.sql"
+  printf "mysql --user=\"root\" --password=\"root\" < \"./test_data_insert.sql\"\n"
+  mysql --user="root" --password="root" < "./test_data_insert.sql"
+
+  cd ..
+
+  node index.js
 fi
-
-cd database
-
-printf "mysql --user=\"root\" --password=\"root\" < \"./drop_database.sql\"\n"
-mysql --user="root" --password="root" < "./drop_database.sql"
-printf "mysql --user=\"root\" --password=\"root\" < \"./database_schema.sql\"\n"
-mysql --user="root" --password="root" < "./database_schema.sql"
-printf "mysql --user=\"root\" --password=\"root\" < \"./test_data_insert.sql\"\n"
-mysql --user="root" --password="root" < "./test_data_insert.sql"
-
-cd ..
-
-node index.js
-
-
-
-
