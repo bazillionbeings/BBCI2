36a400ca6a823583556f10f4ab2700a45a61c86d
Ara Yeressian <ayeressian2@gmail.com>
2015-12-09 19:39:42 +0400

    .

--- index.js
+++ index.js
@@ -26,6 +26,13 @@ let server = http.createServer(handleRequest);
 //server.listen(PORT);
 
 childProcess.exec(__dirname + '/pull.sh ~/BBCentralizedDB', function(err, stdout, stderr) {
-    console.error(stderr);
-    console.log(stdout);
+    if (err) {
+        console.log('err');
+        console.error(err);
+    }
+    if (stderr) {
+        console.log('stderr');
+        console.error(stderr);
+    }
+    if (stdout) console.log(stdout);
 });
