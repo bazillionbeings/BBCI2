c6fa0306e6de8dbb303c5210acefa07a01ae0084
Ara Yeressian <ayeressian2@gmail.com>
2015-12-09 13:23:26 +0400

    fixed logging

--- pull.sh
+++ pull.sh
@@ -4,25 +4,32 @@ set -e
 APP_ADDRESS=$1
 
 if [ "$APP_ADDRESS" = "" ]; then
-  echo "Please provide application address as a first argument."
+  printf "Please provide application address as a first argument."
   exit 1
 fi
 
 cd $APP_ADDRESS
 
-echo "\nkilling node process"
+printf "\nkillall node"
 killall node
 
-echo "\npulling"
+printf "\ngit fetch origin"
 git fetch origin
+printf "\ngit checkout dev"
 git checkout dev
+printf "\ngit diff origin/dev"
 if [[ $(git diff origin/dev) ]]; then
-    git rebase origin/dev
+  printf "\ngit rebase origin/dev"
+  git rebase origin/dev
 fi
 
 cd database
+
+printf "\nmysql -u \"root\" -p \"root\" < \"./drop_database.sql\""
 mysql -u "root" -p "root" < "./drop_database.sql"
+printf "\nmysql -u \"root\" -p \"root\" < \"./database_schema.sql\""
 mysql -u "root" -p "root" < "./database_schema.sql"
+printf "\nmysql -u \"root\" -p \"root\" < \"./test_data_insert.sql\""
 mysql -u "root" -p "root" < "./test_data_insert.sql"
 
 cd ..
