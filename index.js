7eab98099470ba01c7f429cf1ce05ccb727cbd45
Ara Yeressian <ayeressian2@gmail.com>
2015-12-09 17:18:13 +0400

    dasfas

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
 'use strict';
 
 let http = require('http'),
-    childProcess = require('child_process');
+    spawn = require('child_process').spawn;
 
 const PORT = 4333;
 
 function handleRequest(request, res) {
-    console.log('pull request');
+    /*console.log('pull request');
     childProcess.execFile(__dirname + '/pull.sh ~/BBCentralizedDB', function(err, stdout, stderr) {
         console.error(stderr);
         console.log(stdout);
     });
-    res.end();
+    res.end();*/
+
+    let command = spawn(__dirname + '/pull.sh ~/BBCentralizedDB');
+    //let output  = [];
+
+    command.stdout.on('data', function(chunk) {
+        //output.push(chunk);
+        console.log(chunk);
+    });
+
+    command.on('close', function(code) {
+        if (code === 0) {
+            //res.send(Buffer.concat(output));
+        }
+        else {
+            console.log('failed');
+            //res.send(500); // when the script fails, generate a Server Error HTTP response
+        }
+    });
 }
 
 let server = http.createServer(handleRequest);
