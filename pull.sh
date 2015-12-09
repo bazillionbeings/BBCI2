7e340c044b2ca72535c73022e1b6abb3adad7ef2
Ara Yeressian <ayeressian2@gmail.com>
2015-12-09 13:29:02 +0400

    fix exit

--- pull.sh
+++ pull.sh
@@ -1,35 +1,35 @@
 #!/bin/bash
-set -e
+#set +e
 
 APP_ADDRESS=$1
 
 if [ "$APP_ADDRESS" = "" ]; then
-  printf "Please provide application address as a first argument."
+  printf "Please provide application address as a first argument.\n"
   exit 1
 fi
 
 cd $APP_ADDRESS
 
-printf "\nkillall node"
+printf "killall node\n"
 killall node
 
-printf "\ngit fetch origin"
+printf "git fetch origin\n"
 git fetch origin
-printf "\ngit checkout dev"
+printf "git checkout dev\n"
 git checkout dev
-printf "\ngit diff origin/dev"
+printf "git diff origin/dev\n"
 if [[ $(git diff origin/dev) ]]; then
-  printf "\ngit rebase origin/dev"
+  printf "git rebase origin/dev\n"
   git rebase origin/dev
 fi
 
 cd database
 
-printf "\nmysql -u \"root\" -p \"root\" < \"./drop_database.sql\""
+printf "mysql -u \"root\" -p \"root\" < \"./drop_database.sql\"\n"
 mysql -u "root" -p "root" < "./drop_database.sql"
-printf "\nmysql -u \"root\" -p \"root\" < \"./database_schema.sql\""
+printf "mysql -u \"root\" -p \"root\" < \"./database_schema.sql\"\n"
 mysql -u "root" -p "root" < "./database_schema.sql"
-printf "\nmysql -u \"root\" -p \"root\" < \"./test_data_insert.sql\""
+printf "mysql -u \"root\" -p \"root\" < \"./test_data_insert.sql\"\n"
 mysql -u "root" -p "root" < "./test_data_insert.sql"
 
 cd ..
