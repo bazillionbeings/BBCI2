45dd02e375206e5cf7a18293712d3643a199e804
Ara Yeressian <ayeressian2@gmail.com>
2015-12-14 13:41:59 +0400

    .

--- index.js
+++ index.js
@@ -9,23 +9,15 @@ const PORT = 4333;
 
 function handleRequest(request, res) {
     console.log('pull request');
-    childProcess.exec(__dirname + '/pull.sh ~/BBCentralizedDB', function(err, stdout, stderr) {
-        if (err) {
-            console.log('err');
-            console.error(err);
-        }
-        if (stderr) {
-            console.log('stderr');
-            console.error(stderr);
-        }
-        if (stdout) console.log(stdout);
-    });
-    res.end();
+    exec(__dirname + '/pull.sh /home/ubuntu/BBCentralizedDB', function(code, output) {
+      console.log(output);
+      res.end();
+    });    
 }
 
 let server = http.createServer(handleRequest);
 
-//server.listen(PORT);
+server.listen(PORT);
 
 // childProcess.exec(__dirname + '/pull.sh ~/BBCentralizedDB', function(err, stdout, stderr) {
 //     if (err) {
@@ -57,11 +49,6 @@ let server = http.createServer(handleRequest);
 //         console.log(stdout);
 //     }
 // });
-
-exec(__dirname + '/pull.sh /home/ubuntu/BBCentralizedDB', function(code, output) {
-  console.log('Exit code:', code);
-  console.log('Program output:', output);
-});
 
 process.on('uncaughtException', function(err) {
     console.log('Caught exception: ' + err);
